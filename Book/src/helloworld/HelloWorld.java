d437800b0c0b0981b094be3eccc6eb8a90f52c98
VictorStankov <viktorstankov1@gmail.com>
2021-10-22 00:10:09 +0300

    Move files to appropriate folder

---
+++ Book/src/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int myLength = "Hello World".length();
+        System.out.println(myLength);
+    }
+}
