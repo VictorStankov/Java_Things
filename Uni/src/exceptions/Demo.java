f3f66af58cb98f36bb0612a5b70e578e4f58fed7
VictorStankov <viktorstankov1@gmail.com>
2021-10-29 09:20:36 +0300

    Some code cleanup

--- Uni/src/exceptions/Demo.java
+++ Uni/src/exceptions/Demo.java
@@ -3,9 +3,8 @@ package exceptions;
 public class Demo {
     public static void main(String[] args){
         Person first = new Person("John", 5);
-        Person second = null;
 
-        first.compareAge(first, second);
+        first.compareAge(first, null);
         System.out.println(first.convertToText(6));
     }
 }
